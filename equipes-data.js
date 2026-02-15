3ba96a0c217661765aadd8c0ce8fd23d6288488a
Nantuko888 <pierre.venante@gmail.com>
2026-02-15 17:44:19 +0100

    Update equipes-data.js

--- equipes-data.js
+++ equipes-data.js
@@ -1,1 +1,19 @@
-
+const equipesData = {
+  updatedAt: "2026-02-15",
+  teams: [
+    {
+      name: "Équipe 1",
+      members: [
+        "Nom Prénom",
+        "Nom Prénom"
+      ]
+    },
+    {
+      name: "Équipe 2",
+      members: [
+        "Nom Prénom",
+        "Nom Prénom"
+      ]
+    }
+  ]
+};
